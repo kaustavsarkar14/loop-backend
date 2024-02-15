a255137192c68faea1a4c2fe06e87eb3ca587c05
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-02-15 14:33:44 +0530

    cloudinary added in frontend

--- schema/Post.js
+++ schema/Post.js
@@ -3,9 +3,6 @@ import mongoose from "mongoose"
 const postSchema = new mongoose.Schema({
     title : {
         type: String,
-        required : true,
-        minLength: 3,
-        maxLength: 50
     },
     image: {
         type: String,
