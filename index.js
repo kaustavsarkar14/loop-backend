b4120a256e239cbb92c8517074defbeb0b88b282
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-02-27 19:02:49 +0530

    removed some packages

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
-import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import helmet from "helmet";
@@ -34,7 +33,6 @@ export const instance = new Razorpay({
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-// app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
